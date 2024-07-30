403566b7da6220390c11204f7eaa07e3c4ed982f
GBBBAS <bryce.bartmann@shell.com>
2024-07-30 11:01:18 +0100

    Use TimeSeries Query Builder

--- queries/Latest/latest.py
+++ queries/Latest/latest.py
@@ -1,17 +1,18 @@
 from rtdip_sdk.authentication.azure import DefaultAuth
 from rtdip_sdk.connectors import DatabricksSQLConnection
-from rtdip_sdk.queries import latest
+from rtdip_sdk.queries import TimeSeriesQueryBuilder
 
 auth = DefaultAuth().authenticate()
 token = auth.get_token("2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default").token
 connection = DatabricksSQLConnection("{server_hostname}", "{http_path}", token)
 
-parameters = {
-    "business_unit": "{business_unit}",
-    "region": "{region}", 
-    "asset": "{asset_name}", 
-    "data_security_level": "{security_level}", 
-    "tag_names": ["{tag_name_1}", "{tag_name_2}"],
-}
-x = latest.get(connection, parameters)
-print(x)
+data = (
+    TimeSeriesQueryBuilder()
+    .connect(connection)
+    .source("{tablename_or_path}")
+    .latest(
+        tagname_filter=["{tag_name_1}", "{tag_name_2}"],
+    )
+)
+
+print(data)
