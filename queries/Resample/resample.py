9cb2cd624cfc6d8a1b5d79b1349bed832b677bb4
GBBBAS <42962356+GBBBAS@users.noreply.github.com>
2024-07-30 11:03:04 +0100

    Use TimeSeries Query Builder (#22)

--- queries/Resample/resample.py
+++ queries/Resample/resample.py
@@ -1,24 +1,22 @@
 from rtdip_sdk.authentication.azure import DefaultAuth
 from rtdip_sdk.connectors import DatabricksSQLConnection
-from rtdip_sdk.queries import resample
+from rtdip_sdk.queries import TimeSeriesQueryBuilder
 
 auth = DefaultAuth().authenticate()
 token = auth.get_token("2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default").token
 connection = DatabricksSQLConnection("{server_hostname}", "{http_path}", token)
+data = (
+    TimeSeriesQueryBuilder()
+    .connect(connection)
+    .source("{tablename_or_path}")
+    .resample(
+        tagname_filter=["{tag_name_1}", "{tag_name_2}"],
+        start_date="2023-01-01",
+        end_date="2023-01-31",
+        time_interval_rate="15",
+        time_interval_unit="minute",
+        agg_method="first",
+    )
+)
 
-parameters = {
-    "business_unit": "{business_unit}",
-    "region": "{region}", 
-    "asset": "{asset_name}", 
-    "data_security_level": "{security_level}", 
-    "data_type": "float",
-    "tag_names": ["{tag_name_1}", "{tag_name_2}"],
-    "start_date": "2023-01-01",
-    "end_date": "2023-01-31",
-    "time_interval_rate": "15",
-    "time_interval_unit": "minute",
-    "agg_method": "first",
-    "include_bad_data": True,
-}
-x = resample.get(connection, parameters)
-print(x)
+print(data)
--- queries/Time-Weighted-Average/time_weighted_average.py
+++ queries/Time-Weighted-Average/time_weighted_average.py
@@ -1,25 +1,23 @@
 from rtdip_sdk.authentication.azure import DefaultAuth
 from rtdip_sdk.connectors import DatabricksSQLConnection
-from rtdip_sdk.queries import time_weighted_average
+from rtdip_sdk.queries import TimeSeriesQueryBuilder
 
 auth = DefaultAuth().authenticate()
 token = auth.get_token("2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default").token
 connection = DatabricksSQLConnection("{server_hostname}", "{http_path}", token)
 
-parameters = {
-    "business_unit": "{business_unit}",
-    "region": "{region}", 
-    "asset": "{asset_name}", 
-    "data_security_level": "{security_level}", 
-    "data_type": "float",
-    "tag_names": ["{tag_name_1}", "{tag_name_2}"],
-    "start_date": "2023-01-01",
-    "end_date": "2023-01-31",
-    "time_interval_rate": "15",
-    "time_interval_unit": "minute",
-    "window_length": 1,
-    "include_bad_data": True,
-    "step": "true"
-}
-x = time_weighted_average.get(connection, parameters)
-print(x)
+data = (
+    TimeSeriesQueryBuilder()
+    .connect(connection)
+    .source("{tablename_or_path}")
+    .time_weighted_average(
+        tagname_filter=["{tag_name_1}", "{tag_name_2}"],
+        start_date="2023-01-01",
+        end_date="2023-01-31",
+        time_interval_rate="15",
+        time_interval_unit="minute",
+        step="true",
+    )
+)
+
+print(data)
